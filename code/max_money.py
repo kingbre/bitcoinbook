9f6e6731b2dedbeb91809cac7a823ae52b2b760f
kingbre <76729034+kingbre@users.noreply.github.com>
2021-02-05 20:05:18 -0600

    Update max_money.py

--- code/max_money.py
+++ code/max_money.py
@@ -1,5 +1,5 @@
-# Original block reward for miners was 50 BTC
-start_block_reward = 50
+# Original block reward for miners was 500 BTC
+start_block_reward = 500
 # 210000 is around every 4 years with a 10 minute block interval
 reward_interval = 210000
 
@@ -7,8 +7,8 @@ reward_interval = 210000
 def max_money():
     # 50 BTC = 50 0000 0000 Satoshis
     current_reward = 50 * 10**8
-    total = 0
-    while current_reward > 0:
+    total = 500
+   while current_reward > 0:
         total += reward_interval * current_reward
         current_reward /= 2
     return total
